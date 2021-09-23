e9925fdd9be1e2bfb4363ed6aaf54e161ba2dfbd
sbuDiction <nsbu68501@gmail.com>
2021-09-23 07:52:08 +0200

    added a feature

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+// import './App.css';
+import { Container, Row, Col } from 'bootstrap-4-react';
+import HomeScreen from './pages/HomeScreen';
+
+function App() {
+  return (
+    <Container >
+      <HomeScreen />
+    </Container>
+  );
+}
+
+export default App;
